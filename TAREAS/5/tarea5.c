7fec5b8086d9971900619c5f590b546b59b3d6d4
LauVane-FIUNAM <54125757+LauVane-FIUNAM@users.noreply.github.com>
2019-10-30 00:54:19 -0600

    tarea5

---
+++ TAREAS/5/tarea5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int valor(char n){
+	int z;
+	switch(n){
+	case 'J':
+	 z=10;
+	 break;
+	case 'Q':
+	 z=10;
+	 break;
+	case 'K':
+	 z=10;
+	 break;
+	case 'A':
+	 break;
+	case 'j':
+	 z=10;
+	 break;
+	case 'q':
+	 z=10; 
+	 break;
+	case 'k':
+	 z=10;
+	 break;
+	case 'a':
+	 z=11;
+	 break;
+	   
+  }
+  return z;
+
+ }
