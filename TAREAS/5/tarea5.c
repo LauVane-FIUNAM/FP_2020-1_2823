99ee709d86f96affcf415a3bacd52459ae8351e7
LauVane-FIUNAM <54125757+LauVane-FIUNAM@users.noreply.github.com>
2019-10-30 21:45:09 -0600

    Update tarea5.c

--- TAREAS/5/tarea5.c
+++ TAREAS/5/tarea5.c
@@ -32,3 +32,20 @@ int valor(char n){
   return z;
 
  }
+int main(int argc,char* argu[]){
+	int imprimir,
+	coi; 
+	for(int i=1;i<argc;i++){
+		coi=argu[i][0]=='J'||argu[i][0]=='j'||
+		argu[i][0]=='Q'||argu[i][0]=='q'||argu[i][0]=='K'
+		||argu[i][0]=='k'||argu[i][0]=='A'||argu[i][0]=='a';
+		if (coi==1){
+			imprimir=valor(argu[i][0]);
+		}
+		else{
+			imprimir=atoi(argu[i]);
+		}
+		printf("%i\n",imprimir);
+	}
+	return 0;
+}
